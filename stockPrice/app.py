8d04b42d3387edf3fc0a4180e824fa7ff5b133d7
saranga thathsara <saranga.2019952@iit.ac.lk>
2021-05-26 23:45:35 +0530

    Update app.py

--- stockPrice/app.py
+++ stockPrice/app.py
@@ -17,5 +17,8 @@ tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period="1d", start="2010-5-26", end="2021-5-26")
 
 # open high low close volume dividends stock spilts
+st.line_chart(tickerDf.Open)
 st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.High)
+st.line_chart(tickerDf.Low)
 st.line_chart(tickerDf.Volume)
