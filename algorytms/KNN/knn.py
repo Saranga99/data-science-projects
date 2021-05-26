d39fc68547d9e7cfbbee94858fb0290f2a69b6d9
saranga thathsara <saranga.2019952@iit.ac.lk>
2021-05-26 18:10:37 +0530

    adding files
    testing with iris dataset

---
+++ algorytms/KNN/knn.py
@@ -0,0 +1,16 @@
+
+
+
+class KNN:
+    # default value is 3 , number of neghibours
+    def __init__(self,k=3):
+        self.k=k
+    
+    # train method
+    def fit(self,X,y):
+        pass
+    # test method 
+    
+    def predict(self,X):
+        pass
+    
---
+++ algorytms/KNN/knntest.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+cmap = ListedColormap(["#FF0000", "#00FF00", "#0000FF"])
+
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=12345)
+
+# look into dataset
+# print(X.shape)
+# print(X_train[0])
+# print(y_train.shape)
+# print(y_train)
+
+
+# plotting dataset
+# plt.figure()
+# plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap, edgecolor="k", s=20)
+# plt.show()
