cd90a1995491b19798d6dcd84d1165a399714546
saranga thathsara <saranga.2019952@iit.ac.lk>
2021-05-29 15:34:32 +0530

    updated : Linear Regression

--- algorithms/Linear Regression/linear_regression.py
+++ algorithms/Linear Regression/linear_regression.py
@@ -1,18 +1,37 @@
+import numpy as np
 
 
 class LinearRegression:
     # default learining rate is 0.001 (small value), no of iterations in gradient method
-    def __init__(self, learnigRate=0.001, noIteratons=1000):
-        self.learnigRate = learnigRate
-        self.noIterations = noIteratons
+    def __init__(self, learnig_rate=0.001, number_iterations=1000):
+        self.learnig_rate = learnig_rate
+        self.number_iterations = number_iterations
         self.weights = None
         self.bias = None
 
     # fit method which takes training samples and labels
     def fit(self, X, y):
-        pass
+        # implement gradient decent method
+        number_samples, number_features = X.shape
+        # initilize all the weights with zero
+        # each component we put zero
+        self.weights = np.zeros(number_features)
+        self.bias = 0
+
+        # gradient decent
+        for _ in range(self.number_iterations):
+            # approximation
+            y_predicted = np.dot(X, self.weights)+self.bias
+
+            dw = (1/number_samples)*np.dot(X.T, (y_predicted-y))
+            db = (1/number_samples)*np.sum(y_predicted-y)
+
+            # update weight
+            self.weights = self.weights-self.learnig_rate*dw
+            # update bias
+            self.bias = self.bias-self.learnig_rate*db
 
     # predict method which takes the testing samples
-
     def predict(self, X):
-        pass
+        y_predicted = np.dot(X, self.weights)+self.bias
+        return y_predicted
--- algorithms/Linear Regression/linear_regression_test.py
+++ algorithms/Linear Regression/linear_regression_test.py
@@ -1,3 +1,4 @@
+from linear_regression import LinearRegression
 from matplotlib import colors
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -11,6 +12,47 @@ X, y = datasets.make_regression(
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=1234)
 
+# plotting scatter plot
+# fig = plt.figure(figsize=(8, 6))
+# plt.scatter(X[:, 0], y, color="b", marker="o", s=30)
+# plt.show()
+
+
+# testing
+# count = 0
+# while count <= 1:
+#     regressor = LinearRegression(learnig_rate=count)
+#     regressor.fit(X_train, y_train)
+#     Predicted = regressor.predict(X_test)
+#     acc = np.sum(Predicted == y_test)
+#     # mean square error
+
+#     def mean_square_error(y_true, y_predicted):
+#         return np.mean((y_true-y_predicted)**2)
+#     mse = mean_square_error(y_test, Predicted)
+#     print(mse)
+#     count += 0.001
+
+
+regressor = LinearRegression(learnig_rate=0.001)
+regressor.fit(X_train, y_train)
+Predicted = regressor.predict(X_test)
+acc = np.sum(Predicted == y_test)
+# mean square error
+
+
+def mean_square_error(y_true, y_predicted):
+    return np.mean((y_true-y_predicted)**2)
+
+
+mse = mean_square_error(y_test, Predicted)
+print(mse)
+
+# plotting
+y_predicred_line = regressor.predict(X)
+cmap = plt.get_cmap("viridis")
 fig = plt.figure(figsize=(8, 6))
-plt.scatter(X[:, 0], y, color="b", marker="o", s=30)
+m1 = plt.scatter(X_train, y_train, color=cmap(0.9), s=10)
+m2 = plt.scatter(X_test, y_test, color=cmap(0.5), s=10)
+plt.plot(X, y_predicred_line, color="black", linewidth=2, label="rediction")
 plt.show()
