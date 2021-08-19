433eade8b5933910232f337c270411f2b975c2cb
Saranga99 <thathsarasaranga12345@gmail.com>
2021-08-19 22:47:44 +0530

    Update application.py

---
+++ pca_streamlit_app/application.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+import plotly.express as px
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
+
+
