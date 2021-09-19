abe3afdb2280ccd8f06c49407da7bc7df86a28c5
Saranga99 <thathsarasaranga12345@gmail.com>
2021-09-20 01:51:47 +0530

    diabetes app

--- ML-workshop/day30/streamlit_app.py
+++ ML-workshop/day30/streamlit_app.py
@@ -1,16 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[15]:
 
 
 # import pip
 # pip.main(["install","streamlit"])
-
-
-# In[16]:
-
-
 import streamlit as st
 from PIL import Image
 import pandas as pd
@@ -19,25 +10,32 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 
-# In[17]:
-
 
 df=pd.read_csv("diabetes.csv")
-
-
-# In[18]:
-
-
 df.head()
+st.header("Diabetes Detection Application")
+st.write("Diabetes is a disease that occurs when your blood glucose, also called blood sugar, is too high. Blood glucose is your main source of energy and comes from the food you eat. Insulin, a hormone made by the pancreas, helps glucose from food get into your cells to be used for energy.")
+image=Image.open("diabetes.jpg")
 
 
-# In[19]:
+st.image(image)
+st.write("")
+st.subheader("Dataset")
+st.dataframe(df)
+
+st.subheader("Descriptive")
+st.write(df.describe())
 
 
-st.header("Diabets Detection App")
 
 
-# In[ ]:
+
+
+
+
+
+
+
 
 
 
