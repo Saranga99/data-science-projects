9cf8c4cc066fa22a1a2a95158e7132baac5d7eb2
Saranga99 <thathsarasaranga12345@gmail.com>
2021-09-25 02:24:51 +0530

    Update streamlit_app.py

--- ML-workshop/day30/streamlit_app.py
+++ ML-workshop/day30/streamlit_app.py
@@ -10,21 +10,72 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 
-
+#dataset
 df=pd.read_csv("diabetes.csv")
 df.head()
 st.header("Diabetes Detection Application")
 st.write("Diabetes is a disease that occurs when your blood glucose, also called blood sugar, is too high. Blood glucose is your main source of energy and comes from the food you eat. Insulin, a hormone made by the pancreas, helps glucose from food get into your cells to be used for energy.")
 image=Image.open("diabetes.jpg")
 
-
+#image
 st.image(image)
 st.write("")
 st.subheader("Dataset")
 st.dataframe(df)
 
+#sub header
 st.subheader("Data Description")
-st.write(df.describe())
+st.write(df.iloc[:,:8].describe())
+
+#machine learning
+st.subheader("Machine Learning")
+X=df.iloc[:,:8].values
+y=df.iloc[:,8].values
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
+model=RandomForestClassifier(n_estimators=500)
+model.fit(X_train,y_train)
+y_pred=model.predict(X_test)
+acc=accuracy_score(y_test,y_pred)
+st.write("Accuracy Score of RandomForestClassifier : ",acc*100," %")
+
+#user inputs
+st.subheader("Check Your Diabetes Status")
+
+#st.slider(name , min , max , default)
+preg=st.slider("Pregnancies",0,20,0)
+glu=st.slider("Glucose Level",0,200,0)
+bp=st.slider("Blood Preasure",0,130,0)
+skin=st.slider("Skin Thickness",0,100,0)
+ins=st.slider("Insulin Level",0.0,1000.0,0.0)
+bmi=st.slider("BMI",0.0,70.0,0.0)
+dpf=st.slider("DPF ",0.000,3.000,0.000)
+age=st.slider("Age",0,100,0)
+
+
+#inputes for the model
+inputs={"Pregnancies":preg,
+        "Glucose Level":glu,
+        "Blood Preasure":bp,
+        "Skin Thickness":skin,
+        "Insulin Level":ins,
+        "BMI":bmi,
+        "DPF":dpf,
+        "Age":age}
+
+#user inputs as a dataframe
+userInputs=pd.DataFrame(inputs,index=["User Inputs"])
+
+st.write("User Inputs")
+st.write(userInputs)
+
+
+
+
+
+
+
+
+
 
 
 
