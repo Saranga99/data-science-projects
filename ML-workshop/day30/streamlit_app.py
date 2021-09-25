d9875f4747e47a1b9895b9651c4825c14a7192f9
Saranga99 <thathsarasaranga12345@gmail.com>
2021-09-25 09:37:09 +0530

    Update streamlit_app.py

--- ML-workshop/day30/streamlit_app.py
+++ ML-workshop/day30/streamlit_app.py
@@ -123,7 +123,87 @@ else:
 
 
 
+# #test
+# import streamlit as st
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# from sklearn import datasets
+# from sklearn.model_selection import train_test_split
+# from sklearn.decomposition import PCA
+# from sklearn.svm import SVC
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.naive_bayes import MultinomialNB
+# from sklearn.metrics import accuracy_score
+# import plotly.express as px
 
+
+
+# st.write("""
+# # Explore different classifier with BARG dataset
+# Which one is the best?
+# """)
+
+# dataset_name = "Sentiment"
+# classifier_name = st.sidebar.selectbox(
+#     'Select classifier',
+#     ('KNN', 'SVM', 'Random Forest', "Naive Bayes")
+# )
+
+
+
+
+
+# def add_parameter_ui(clf_name):
+#     params = dict()
+#     if clf_name == 'SVM':
+#         C = st.sidebar.slider('C', 0.01, 10.0)
+#         params['C'] = C
+#     elif clf_name == 'KNN':
+#         K = st.sidebar.slider('K', 1, 15)
+#         params['K'] = K
+#     elif clf_name == 'Naive Bayes':
+#         alpha = st.sidebar.slider('alpha', 0.0, 10.0)
+#         params['alpha'] = alpha
+#     else:
+#         max_depth = st.sidebar.slider('max_depth', 2, 15)
+#         params['max_depth'] = max_depth
+#         n_estimators = st.sidebar.slider('n_estimators', 1, 500)
+#         params['n_estimators'] = n_estimators
+#     return params
+
+
+# params = add_parameter_ui(classifier_name)
+
+
+# def get_classifier(clf_name, params):
+#     clf = None
+#     if clf_name == 'SVM':
+#         clf = SVC(C=params['C'])
+#     elif clf_name == 'KNN':
+#         clf = KNeighborsClassifier(n_neighbors=params['K'])
+#     elif clf_name == 'Naive Bayes':
+#         clf = MultinomialNB(alpha=params['alpha'])
+#     else:
+#         clf = clf = RandomForestClassifier(n_estimators=params['n_estimators'],
+#                                            max_depth=params['max_depth'], random_state=1234)
+#     return clf
+
+
+# clf = get_classifier(classifier_name, params)
+# #### CLASSIFICATION ####
+
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X, y, test_size=0.2, random_state=0)
+
+# clf.fit(X_train, y_train)
+# y_pred = clf.predict(X_test)
+
+# acc = accuracy_score(y_test, y_pred)
+
+# st.write(f'Classifier = {classifier_name}')
+# st.write(f'Accuracy =', acc)
 
 
 
