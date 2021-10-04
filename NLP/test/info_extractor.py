87b06e572c165e93a87057cfd9274643d3438704
Saranga99 <thathsarasaranga12345@gmail.com>
2021-10-04 21:06:31 +0530

    adding model to the CLI

---
+++ NLP/test/info_extractor.py
@@ -0,0 +1,111 @@
+# D:\iit\CV\Saranga Kumarapeli.pdf
+from transformers import pipeline
+from pdfminer.high_level import extract_text
+import nltk
+import re
+
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('maxent_ne_chunker')
+# nltk.download('words')
+# import pdftotext
+print("\n_____Resume Information Extractor - CLI based Application_____\n")
+
+# extract all textual infomation in high_level
+
+
+def extract_text_from_pdf(pdf_path):
+    return extract_text(pdf_path)
+
+
+def load_qa_model():
+    model = pipeline("question-answering")
+    return model
+
+
+# extract all names in input text and return as a list
+def extract_names(txt):
+    person_names = []
+
+    for sent in nltk.sent_tokenize(txt):
+        for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
+            if hasattr(chunk, 'label') and chunk.label() == 'PERSON':
+                person_names.append(
+                    ' '.join(chunk_leave[0] for chunk_leave in chunk.leaves())
+                )
+
+    return person_names
+
+
+# email - regular expression
+EMAIL_REG = re.compile(r'[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+')
+
+
+# extract all emails in input text
+def extract_emails(resume_text):
+    return re.findall(EMAIL_REG, resume_text)
+
+
+# mobile number
+def extract_mobile_numbers(resume_text):
+    phone = []
+    for i in re.findall(r'(\s+([0-9]+\s+)+)', resume_text):
+        phone.append(i)
+    return phone
+
+
+print("Please wait buddy... I'm Loading..... :D")
+qa = load_qa_model()
+
+
+while True:
+    try:
+        path = input("Enter Your path Here : ")
+        lines = [extract_text_from_pdf(path)]
+        with open('readme.txt', 'w') as f:
+            for line in lines:
+                f.write(line)
+                # f.write('')
+        break
+    except:
+        print("\nPlease Enter Correct Path\n")
+
+# open written text file and load in a variable
+with open("readme.txt", "r") as myfile:
+    data = myfile.read()
+
+
+# calling methods
+names = extract_names(data)
+if names:
+    print("\n____Extracted Information____\n")
+
+    print("Name           : ", names[3][:-8])
+
+emails = extract_emails(data)
+if emails:
+    print("Email          : ", emails[0])
+
+
+numbers = extract_mobile_numbers(data)
+print("Telephone No.  :", numbers[1][0], "/", numbers[2][0])
+
+
+# QA
+while True:
+    user_input = input(
+        "\nPress 'y' to ask question from cv, press 'q' to exit the application : ")
+    user_input = user_input.lower()
+    if user_input == "y":
+        question = input("\nAsk Your Question : ")
+        try:
+            answers = qa(question=question, context=data)
+            print("\nAnswer : ", answers['answer'],
+                  "\n", "Score : ", answers["score"])
+        except:
+            print("\nsorry..I Couldn't find any answer :D")
+    elif user_input == "q":
+        print("\nThank you for using, Stay safe !!!")
+        break
+    else:
+        print("\nPlease Enter Correct inputs to proceed :D")
