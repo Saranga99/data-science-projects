e1afe33ba8f25c539f0ebd05a355fd98bf464ba8
Saranga99 <thathsarasaranga12345@gmail.com>
2021-10-04 15:11:56 +0530

    Update app.py

--- NLP/test/app.py
+++ NLP/test/app.py
@@ -1,4 +1,5 @@
 # importings
+from transformers import pipeline
 import streamlit as st
 from PIL import Image
 from pdfminer.high_level import extract_text
@@ -89,3 +90,23 @@ st.write("\n")
 st.image(image)
 st.write(
     " [Git Hub](https://github.com/Saranga99/data-science-projects/tree/main/NLP)")
+
+
+@st.cache(allow_output_mutation=True)
+def load_qa_model():
+    model = pipeline("question-answering")
+    return model
+
+
+qa = load_qa_model()
+st.title("Ask Questions about your Text")
+sentence = st.text_area('Please paste your article :', height=30)
+question = st.text_input("Questions from this article?")
+button = st.button("Get me Answers")
+max = st.sidebar.slider('Select max', 50, 500, step=10, value=150)
+min = st.sidebar.slider('Select min', 10, 450, step=10, value=50)
+do_sample = st.sidebar.checkbox("Do sample", value=False)
+with st.spinner("Discovering Answers.."):
+    if button and sentence:
+        answers = qa(question=question, context=sentence)
+        st.write(answers['answer'])
