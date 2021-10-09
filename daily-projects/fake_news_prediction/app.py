b5690e640333602989906b1b0c797ed0bb7ac23c
Saranga99 <thathsarasaranga12345@gmail.com>
2021-10-09 21:07:55 +0530

    Update app.py

--- daily-projects/fake_news_prediction/app.py
+++ daily-projects/fake_news_prediction/app.py
@@ -2,7 +2,20 @@ import pickle
 import streamlit as st
 # convert text in to feature vectors
 from sklearn.feature_extraction.text import TfidfVectorizer
+import re #usful for searching words in pharagraph
+from nltk.corpus import stopwords  #words doesn't add much value to phara (rticals)
+from nltk.stem.porter import PorterStemmer #gives a root word
+from sklearn.feature_extraction.text import TfidfVectorizer #convert text in to feature vectors
 
+
+def stemming(content):
+    port_stem=PorterStemmer()
+    stemmed_content=re.sub("[^a-zA-z]"," ",content)
+    stemmed_content=stemmed_content.lower()
+    stemmed_content=stemmed_content.split()
+    stemmed_content=[port_stem.stem(word) for word in stemmed_content if not word in stopwords.words("english")]
+    stemmed_content= " ".join(stemmed_content)
+    return stemmed_content
 
 #header and description
 st.title("Fake News Classifier")
@@ -15,6 +28,7 @@ classifier = pickle.load(pickle_in)
 @st.cache()
 # defining the function which will make the prediction using the data which the user inputs
 def prediction(text):
+    text=text.apply(stemming)
     # ocnverting data to numerical data
     vectorizer = TfidfVectorizer()
     text = vectorizer.fit_transform(text)
